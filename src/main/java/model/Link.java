3d799aa1da8cc3d8db14227faaba787f82e20f9c
Everton Rocha <imevertonrch@gmail.com>
2025-01-23 02:38:29 -0300

    fix: corrigindo parâmetro de criação da coleção

--- src/main/java/model/Link.java
+++ src/main/java/model/Link.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Document(collation = "links")
+@Document(collection = "links")
 public class Link {
 
     @Id
