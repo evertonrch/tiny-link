c593708c401075cf273e08dec2259c30b3ddf0bf
Everton Rocha <imevertonrch@gmail.com>
2025-01-27 23:20:11 -0300

    refactory: alterando código de status HTTP

--- src/main/java/com/desafios/encurtador_url/exception/LinkExpiradoException.java
+++ src/main/java/com/desafios/encurtador_url/exception/LinkExpiradoException.java
@@ -1,5 +1,6 @@
 package com.desafios.encurtador_url.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 
 public class LinkExpiradoException extends LinkNaoEncontradoException {
@@ -12,6 +13,7 @@ public class LinkExpiradoException extends LinkNaoEncontradoException {
     public ProblemDetail toProblemDetail() {
         ProblemDetail problemDetail = super.toProblemDetail();
         problemDetail.setTitle("Link expirado.");
+        problemDetail.setStatus(HttpStatus.GONE);
 
         return problemDetail;
     }
